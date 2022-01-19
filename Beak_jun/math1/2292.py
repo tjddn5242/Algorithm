d33541c5a87511daf584c009058cbf31b97e548e
Sungwoo <61862332+tjddn5242@users.noreply.github.com>
2022-01-19 14:51:49 +0000

    22.01.19

--- Beak_jun/math1/2292.py
+++ Beak_jun/math1/2292.py
@@ -1,8 +1,14 @@
-list_ = [1]
-a=1
-b=2
-while a<1000:
-  a=a+b
-  list_.append(a)
-  b+=6
-print(list_)
+k=int(input())
+if k==1:
+  print(1)
+else:
+  list_ = [7]
+  a=7
+  b=12
+  i=2
+  while a<k:
+    a=a+b
+    list_.append(a)
+    b+=6
+    i+=1
+  print(i)
