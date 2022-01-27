b670bc79a88de3457aaf179d37ddd226a4c599b5
Sungwoo <61862332+tjddn5242@users.noreply.github.com>
2022-01-27 06:29:33 +0000

    22.01.27

--- Beak_jun/math1/1011.py
+++ Beak_jun/math1/1011.py
@@ -1,14 +1,14 @@
 T = int(input())
+LL = [1,2]
 for _ in range(T):
   x,y = map(int, input().split())
   k=y-x
-  i=0
-  while True:
-    if i**2>k:
-      break
-    else:
-      i+=1
-  if i**2-i>k:
-    print(i-1)
+  if k==1 or k==2:
+    print(LL[k-1])
   else:
-    print(i)
+    j=2
+    for i in range(3,k+1,2):
+      LL.extend([i for _ in range(j)])
+      LL.extend([i+1 for _ in range(j)])
+      j += 1
+    print(LL[k-1])
